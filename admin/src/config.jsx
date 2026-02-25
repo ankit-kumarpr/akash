25eece14a9239c7707652f3442ab0bff0fd007f5
ankit-kumarpr <146826335+ankit-kumarpr@users.noreply.github.com>
2026-02-25 14:15:25 +0530

    Update config.jsx

--- admin/src/config.jsx
+++ admin/src/config.jsx
@@ -1,5 +1,6 @@
 const config = {
-    backendBaseUrl: "http://localhost:5001"
+    // backendBaseUrl: "http://localhost:5001"
+    backendBaseUrl: "https://akash-backend-75iv.onrender.com"
     // backendBaseUrl: "https://backend-createch.onrender.com"
     //  https://backend-createch.onrender.com
 
